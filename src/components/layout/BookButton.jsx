111ae1aea4335e1f825faee4b5e36a3dd9d400a4
Dhiraj <dkskp2005@gmail.com>
2025-05-23 18:27:37 +0530

    btn wide

--- src/components/layout/BookButton.jsx
+++ src/components/layout/BookButton.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+// const BookButton = () => {
+//   return (
+//     <div className="fixed bottom-2 left-1/2 transform -translate-x-1/2 z-50 md:hidden ">
+//       <button
+//         onClick={() => window.dispatchEvent(new Event("openPopup"))}
+//         className="bg-[#33398B] text-white px-6 py-3 rounded-lg font-semibold hover:bg-[#4a4fa0] transition cursor-pointer"
+//       >
+//         Book a Visit
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default BookButton;
 
 const BookButton = () => {
   return (
-    <div className="fixed bottom-2 left-1/2 transform -translate-x-1/2 z-50 md:hidden ">
+    <div className="fixed bottom-2 left-1/2 transform -translate-x-1/2 z-50 w-full px-4 md:hidden">
       <button
         onClick={() => window.dispatchEvent(new Event("openPopup"))}
-        className="bg-[#33398B] text-white px-6 py-3 rounded-lg font-semibold hover:bg-[#4a4fa0] transition cursor-pointer"
+        className="w-full max-w-md bg-[#33398B] text-white px-6 py-3 rounded-lg font-semibold hover:bg-[#4a4fa0] transition cursor-pointer"
       >
         Book a Visit
       </button>
